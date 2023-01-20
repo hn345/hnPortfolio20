ac06da12870591fe79b66aadc3550e1a932deacc
HannaKim <hn34arte@gmail.com>
2023-01-20 22:02:22 +0900

    2020 버전 포트폴리오

---
+++ js/site.js
@@ -0,0 +1,12 @@
+// 김한나 의 제이쿼리 //
+
+
+
+
+//$('.pf1-btn').click(function(){
+//	$('.pf1-cont').css('display','block')
+//});
+
+$('.pf1-btn').click(function(){
+	$('.pf1-cont').css('top','100')
+});
